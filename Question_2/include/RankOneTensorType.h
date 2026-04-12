d0f64d0283b10afa8ff2959b228074e112433e26
Jacob Czekalla <jczekalla@codeweavers.com>
2026-04-12 19:12:13 -0400

    Add final operators to question 2.
    Question 2 is finished.

--- Question_2/include/RankOneTensorType.h
+++ Question_2/include/RankOneTensorType.h
@@ -7,7 +7,7 @@
 #include "BaseTensor.h"
 
 template<typename T>
-class RankOneTensorType : public BaseTensor 
+class RankOneTensorType : public BaseTensor
 {
     private:
         std::vector<T> data;
@@ -21,16 +21,35 @@ class RankOneTensorType : public BaseTensor
         T get_data_element(int index) const;
         void loadData() override;
 
-        RankOneTensorType<T>& operator++();  
+        RankOneTensorType<T>& operator++();
         RankOneTensorType<T> operator+(const RankOneTensorType<T>& other) const;
         RankOneTensorType<T> operator+(const T& scalar) const;
         RankOneTensorType<T>& operator=(const RankOneTensorType<T>& other);
         T& operator[](int index);
         const T& operator[](int index) const;
 
-        friend std::ostream& operator<<(std::ostream& os, const RankOneTensorType<T> tensor_out);
-        friend std::istream& operator>>(std::istream& is, const RankOneTensorType<T> tensor_in);
+        template<typename U>
+        friend std::ostream& operator<<(std::ostream& os, const RankOneTensorType<U>& tensor_out);
+
+        template<typename U>
+        friend std::istream& operator>>(std::istream& is, RankOneTensorType<U>& tensor_in);
 };
+
+template<typename U>
+std::ostream& operator<<(std::ostream& os, const RankOneTensorType<U>& tensor_out)
+{
+    for (const auto& x : tensor_out.data)
+        os << x << " ";
+    return os;
+}
+
+template<typename U>
+std::istream& operator>>(std::istream& is, RankOneTensorType<U>& tensor_in)
+{
+    for (auto& x : tensor_in.data)
+        is >> x;
+    return is;
+}
 
 template class RankOneTensorType<double>;
 template class RankOneTensorType<char>;
